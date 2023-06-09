d0bbdcd72e65f8375d81cb12a7faadbf273523f6
1AoB <78208268+1AoB@users.noreply.github.com>
2023-06-09 13:00:22 +0800

    Add files via upload

---
+++ 服务端/shared_bike3_second/user_event_handler.cpp
@@ -0,0 +1,91 @@
+#include "user_event_handler.h"
+#include "DispatchMsgService.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+
+UserEventHandler::UserEventHandler() :iEventHandler("UserEventHandler")
+{
+	//未来需要实现订阅事件的处理
+	DispatchMsgService::getInstance()->subscribe(EEVENTID_GET_MOBLIE_CODE_REQ, this);
+	DispatchMsgService::getInstance()->subscribe(EEVENTID_LOGIN_REQ, this);
+	thread_mutex_create(&pm_);
+}
+
+UserEventHandler::~UserEventHandler()
+{
+	//未来需要实现退订事件的处理
+	DispatchMsgService::getInstance()->unsubscribe(EEVENTID_GET_MOBLIE_CODE_REQ, this);
+	DispatchMsgService::getInstance()->unsubscribe(EEVENTID_LOGIN_REQ, this);
+	thread_mutex_destroy(&pm_);
+
+}
+
+
+iEvent* UserEventHandler::handle(const iEvent* ev)
+{
+	if (ev == NULL)
+	{
+		//LOG_ERROR("input ev is NULL");
+		printf("input ev is NULL");
+	}
+
+	u32 eid = ev->get_eid();
+
+	if (eid == EEVENTID_GET_MOBLIE_CODE_REQ)
+	{
+		return handle_mobile_code_req((MobileCodeReqEv*)ev);
+	}
+	else if (eid == EEVENTID_LOGIN_REQ)
+	{
+		//return handle_login_req((LoginEv*) ev);
+	}
+	else if (eid == EEVENTID_RECHARGE_REQ)
+	{
+		//return handle_recharge_req((RechargeEv*) ev);
+	}
+	else if (eid == EEVENTID_GET_ACCOUNT_BALANCE_REQ)
+	{
+		//return handle_get_account_balance_req((GetAccountBalanceEv*) ev);
+	}
+	else if (eid == EEVENTID_LIST_ACCOUNT_RECORDS_REQ)
+	{
+		//return handle_list_account_records_req((ListAccountRecordsReqEv*) ev);
+	}
+
+	return NULL;
+}
+
+MobileCodeRspEv* UserEventHandler::handle_mobile_code_req(MobileCodeReqEv* ev)
+{
+	i32  icode = 0;
+	std::string mobile_ = ev->get_mobile();
+	//LOG_DEBUG("try to get moblie phone %s validate code .", mobile_.c_str());
+	printf("try to get moblie phone %s validate code .\n", mobile_.c_str());
+
+	icode = code_gen();
+	thread_mutex_lock(&pm_);
+	m2c_[mobile_] = icode;
+	thread_mutex_unlock(&pm_);
+	printf("mobile: %s, code: %d\n", mobile_.c_str(), icode);
+
+	return new MobileCodeRspEv(200, icode);
+}
+
+i32 UserEventHandler::code_gen()
+{
+	i32 code = 0;
+	srand((unsigned int)time(NULL));
+
+	code = (unsigned int)(rand() % (999999 - 100000) + 100000);
+
+	return code;
+}
+
+
+
